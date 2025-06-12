1aa4f9950e3a8a68822dde90a602c5759601ca2e
miftahuljannahhh <mj5416386@gmail.com>
2025-06-12 11:19:31 +0700

    simpan angka di queue

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -8,7 +8,7 @@ private:
     int FRONT, REAR;
     int queue_array[5];
 
-    public:
+public:
     Queues()
     {
         FRONT = -1;
@@ -22,18 +22,21 @@ private:
         cin >> num;
         cout << endl;
 
-    // 2. Cek apakah antrian kosong
-    if (FRONT == -1)
-    {
-        FRONT = 0; // 2.a
-        REAR = 0;  // 2.b
-    }
-    else
-    {
-        // Jika benar berada diposisi terakhir array, kembali ke awal array
-        if (REAR == max - 1)
-            REAR = 0;
+        // 2. Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0; // 2.a
+            REAR = 0;  // 2.b
+        }
         else
-            REAR = REAR + 1;
+        {
+            // Jika benar berada diposisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
+
 };
