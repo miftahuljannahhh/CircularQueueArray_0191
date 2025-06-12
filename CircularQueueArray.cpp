7c29420af95fbf4f765c9db137d39e46145ab946
miftahuljannahhh <mj5416386@gmail.com>
2025-06-12 11:10:21 +0700

    void insert

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -15,4 +15,35 @@ private:
         REAR = -1;
     }
 
+    void insert()
+    {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        // 1. Cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n"; // 1.a
+            return;                       // 1.b
+        }
+
+        // 2. Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0; // 2.a
+            REAR = 0;  // 2.b
+        }
+        else
+        {
+            // Jika benar berada diposisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
+
 };
