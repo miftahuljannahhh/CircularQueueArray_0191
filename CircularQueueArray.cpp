83025663855d4ae9cc71ce64fcdacb44928ec46e
miftahuljannahhh <mj5416386@gmail.com>
2025-06-12 11:22:53 +0700

    cek apakah antrian kosong pada function remove

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -47,22 +47,5 @@ public:
             cout << "Queue underflow\n";
             return;
         }
-        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
-
-        // Cek jika antrian hanya memiliki satu elemen
-        if (FRONT == REAR)
-        {
-            FRONT = -1;
-            REAR = -1;
-        }
-        else
-        {
-            // Jika benar berada diposisi terakhir array, kembali ke awal array
-            if (FRONT == max - 1)
-                FRONT = 0;
-            else
-                FRONT = FRONT + 1;
-        }
     }
-
 };
