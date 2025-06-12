94e6686200e8941de4e077d84133e01828ac6455
miftahuljannahhh <mj5416386@gmail.com>
2025-06-12 11:30:29 +0700

    void display

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -64,4 +64,50 @@ public:
                 FRONT = FRONT + 1;
         }
     }
+
+    void display()
+    {
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        // Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queue are...\n";
+
+    // Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+    if (FRONT_position <= REAR_position)
+    {
+        while (FRONT_position <= REAR_position)
+        {
+            cout << queue_array[FRONT_position] << " ";
+            FRONT_position++;
+        }
+        cout << endl;
+    }
+    else
+    {
+        // Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga akhir REAR
+        while (FRONT_position <= max - 1)
+        {
+            cout << queue_array[FRONT_position] << " ";
+            FRONT_position++;
+        }
+
+        FRONT_position = 0;
+
+        // Iterasi dari awal array hingga REAR
+        while (FRONT_position <= REAR_position)
+        {
+            cout << queue_array[FRONT_position] << " ";
+            FRONT_position++;
+        }
+        cout << endl;
+    }
+}
 };
+
